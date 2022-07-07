20ec6f0beb6aeb1abeadf12119ec7bf6d4847d2d
Matthieu Pizenberg <matthieu.pizenberg@fittingbox.com>
2022-07-07 22:14:16 +0200

    Update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,6 @@
+# Use starship prompt
+starship init fish | source
+
 # Add the path to personal scripts to PATH
 add_to_path $HOME/scripts
 
@@ -9,6 +12,8 @@ add_to_path $HOME/.cargo/bin
 
 # Load conda if installed
 add_to_path $HOME/miniconda3/bin
+add_to_path $HOME/anaconda3/bin
+eval conda "shell.fish" "hook" $argv | source
 
 # Load jenv if installed
 add_to_path $HOME/.jenv/bin
@@ -24,8 +29,14 @@ add_to_path /opt/cuda/bin
 # CF that nice article: https://zaiste.net/posts/shell-commands-rust/
 alias cat bat
 alias ls exa
+alias hx helix
+alias ps procs
+alias z zoxide
 # alias grep rg    # better use rg directly actually
 # alias cloc tokei # better use tokei directly actually
 # alias find fd    # better use fd directly actually
 # alias sed sd     # better use sd directly actually
-# alias top ytop   # better use ytop directly actually
+# alias top btm    # better use btm directly actually
+
+# alias du dust        # better use dust directly actually
+# alias time hyperfine # better use ytop directly actually
