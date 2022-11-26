1a128ebded3e19779ebff0a37d963dd7a3a4719f
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2022-11-26 19:46:05 +0100

    Remove a bunch of stuff for my fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,6 +7,9 @@ add_to_path $HOME/programs
 # Add the path to rust binaries to PATH
 add_to_path $HOME/.cargo/bin
 
+# Add the path to go binaries to PATH
+add_to_path $HOME/go/bin
+
 # Load conda if installed
 add_to_path $HOME/miniconda3/bin
 
@@ -20,10 +23,26 @@ add_to_path $HOME/programs/matlab/bin
 # Add Cuda if installed
 add_to_path /opt/cuda/bin
 
+# Use starship prompt
+if type -q starship
+  starship init fish | source
+end
+
+# Use zoxide smarter cd
+if type -q zoxide
+  zoxide init fish | source
+end
+
+# Configure fnm if available (fast node manager)
+if type -q fnm
+  fnm env --use-on-cd | source
+end
+
 # Some aliases for command line tools
 # CF that nice article: https://zaiste.net/posts/shell-commands-rust/
 alias cat bat
 alias ls exa
+alias hx helix
 # alias grep rg    # better use rg directly actually
 # alias cloc tokei # better use tokei directly actually
 # alias find fd    # better use fd directly actually
