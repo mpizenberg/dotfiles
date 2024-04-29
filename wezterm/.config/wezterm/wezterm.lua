65ce265d5bf6c8200527bfed4d9600a5b8b4a28b
Matthieu Pizenberg <matthieu.pizenberg@fittingbox.com>
2024-04-29 11:10:35 +0200

    Update wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,8 +1,11 @@
 local wezterm = require 'wezterm';
 return {
-  default_prog = {"wsl", "zellij", "attach", "--create"},
-  font = wezterm.font("FiraMono Nerd Font"),
-  window_decorations = "RESIZE",
+  -- default_prog = {"wsl", "zellij", "attach", "--create"},
+  -- font = wezterm.font("FiraMono Nerd Font"),
+  -- window_decorations = "RESIZE",
+  font = wezterm.font("UbuntuMono Nerd Font"),
+  font_size = 14,
+  window_decorations = "NONE",
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
   window_close_confirmation = "NeverPrompt",
